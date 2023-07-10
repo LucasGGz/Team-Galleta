178a936824b41d4e7d3d99cefa1965ba682b1a17
Reina-Nerea <reinanerea85@gmail.com>
2023-07-09 18:34:17 -0600

    Deco/Bombas

---
+++ Assets/Scripts/Bombas.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombas : MonoBehaviour
+{
+    public Transform objetivo;
+    public Transform puntoInicio;
+
+private void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.tag == "Player")
+    {
+        collision.gameObject.transform.position = puntoInicio.position;
+        Debug.Log("Tocado por la bomba");
+    }
+}
+}
